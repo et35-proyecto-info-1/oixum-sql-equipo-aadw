e59bb5d54891a739dd8204ef6c6f1bb5eccc2547
Ariel <alan>
2019-11-25 17:28:23 -0300

    Final con pug

--- scripts/crearTabla.sql
+++ scripts/crearTabla.sql
@@ -1,3 +1,4 @@
+﻿DROP DATABASE IF EXISTS test ;
 CREATE DATABASE IF NOT EXISTS test;
 
 CREATE TABLE IF NOT EXISTS test.estudiantes (
@@ -11,6 +12,21 @@ CREATE TABLE IF NOT EXISTS test.estudiantes (
 INSERT INTO test.estudiantes (nombre, comidaPreferida, fechaNacimiento)
 VALUES
   ('Federico Aloi', 'Panqueques de arroz', '1991-10-30'),
-  ('Andrea Sierra Bueno', 'Puré de papas con huevo revuelto', '1985-08-30'),
+  ('Andrea Sierra Bueno', 'Pure de papas con huevo revuelto', '1985-08-30'),
   ('Fernando Cazas', 'Hamburguesas de lentejas', '1963-02-12'),
   ('Marta Lucero', 'Conejo a la cacerola', '1938-01-24');
+
+CREATE TABLE IF NOT EXISTS test.profesores (
+	id INT NOT NULL AUTO_INCREMENT,
+	nombre varchar(100) NOT NULL,
+	Materia varchar(100) NULL,
+	fechaNacimiento DATE NOT NULL,
+	CONSTRAINT estudiantes_PK PRIMARY KEY (id)
+);
+
+INSERT INTO test.profesores (nombre, materia, fechaNacimiento)
+VALUES
+('Juan Perez', 'Matematica', '1951-10-30'),
+  ('Santiago Trini', 'Proyecto Informatico', '1985-10-10'),
+  ('Marina Petean', 'Lengua', '2002-02-12'),
+  ('Alan Williams', 'Fisica', '2002-01-24');
--- src/app.js
+++ src/app.js
@@ -1,27 +1,25 @@
-var express = require('express');
+﻿var express = require('express');
 var connection = require('./connection');
-var exphbs  = require('express-handlebars');
 var path = require('path');
 var app = express();
 
 app.use(express.urlencoded({ extended: false }));
-
+app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+app.set('view engine', 'pug');
 
 
 
 app.get('/', function(req, res) {
-res.render('home');
+res.render('index');
 });
 
 app.post('/', function(req, res) {
   const query = req.body.query;
   connection.query(query, function (error, results, fields) {
-    res.render('home', {
-      resultados: results,
-      columnas:fields,
+    res.render('index', {
+      results: results,
+      fields:fields,
       error: error
     });
   });
